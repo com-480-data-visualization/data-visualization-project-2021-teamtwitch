782372864c9d0399e916829c9fc4a23e4f8f585d
Huan-Cheng Chang <changhc84@gmail.com>
2021-04-22 08:17:35 +0200

    sample files

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Block1 from "src/components/Block1";
+import Block2 from "src/components/Block2";
+
+export default function App(): JSX.Element {
+  return (
+    <div>
+      <Block1 />
+      <hr />
+      <Block2 />
+    </div>
+  );
+}
---
+++ app/src/components/Block2/index.ts
@@ -0,0 +1,3 @@
+import Block2 from "./Block2";
+
+export default Block2;
