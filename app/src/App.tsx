97bd76bac67a926e15c1f029b4366c77729840b9
Huan-Cheng Chang <changhc84@gmail.com>
2021-05-04 22:59:14 +0200

    Bar chart for comparison (#4)
    * basic bar chart
    
    * slider update works
    
    * column selector
    
    * language selector

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,13 @@
 import React from "react";
-import Block1 from "src/components/Block1";
-import Block2 from "src/components/Block2";
-import BlockSampleChart from "src/components/BlockSampleChart";
+import BarChartForComparison from "src/components/BarChartForComparison";
 import BubbleChart from "src/components/BubbleChart";
 
 export default function App(): JSX.Element {
   return (
     <div>
-      <Block1 />
-      <hr />
-      <Block2 />
-      <hr />
-      <BlockSampleChart />
-      <hr />
       <BubbleChart />
+      <hr />
+      <BarChartForComparison />
     </div>
   );
 }
