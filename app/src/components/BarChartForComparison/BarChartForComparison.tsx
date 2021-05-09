99c8245f00c407b0a55db81d8ef3a73e513a0361
Huan-Cheng Chang <changhc84@gmail.com>
2021-05-09 12:17:37 +0200

    add area chart

--- app/src/components/BarChartForComparison/BarChartForComparison.tsx
+++ app/src/components/BarChartForComparison/BarChartForComparison.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import * as d3 from "d3";
 import { sliderBottom } from "d3-simple-slider";
-import { languageMapping, dateLabels, columnLabels } from "./utils";
+import { languageMapping, dateLabels, columnLabels, getDate } from "./utils";
 
 const styles = require("./barchart.scss");
 
-interface DataRow {
-  year: number;
+interface RawDataRow {
+  year: string;
   month: string;
   language: string;
 }
@@ -15,16 +15,21 @@ interface GenericDataType {
   [key: string]: number;
 }
 
+interface DataEntry {
+  date: Date;
+  value: number;
+}
+
 const loadData = async (): Promise<{
-  [key: string]: { [key: string]: { key: string; value: number }[] };
+  [key: string]: { [key: string]: DataEntry[] };
 }> => {
   return fetch(
     "https://raw.githubusercontent.com/com-480-data-visualization/data-visualization-project-2021-teamtwitch/662d2972130af64b87bc9c6325bfd86390e15498/data/agg.json"
   )
     .then((data) => data.json())
-    .then((body: (DataRow & GenericDataType)[]) => {
+    .then((body: (RawDataRow & GenericDataType)[]) => {
       const data: {
-        [key: string]: { [key: string]: { key: string; value: number }[] };
+        [key: string]: { [key: string]: DataEntry[] };
       } = {};
       const colKeys = columnLabels.map((s) => s.toLowerCase().replace(" ", ""));
       for (const code of Object.values(languageMapping)) {
@@ -36,9 +41,7 @@ const loadData = async (): Promise<{
       for (const row of body) {
         for (const col of colKeys) {
           data[row.language][col].push({
-            key: `${row.year} ${
-              row.month[0].toUpperCase() + row.month.slice(1)
-            }`,
+            date: getDate(row.year, row.month),
             value: row[col],
           });
         }
@@ -48,7 +51,7 @@ const loadData = async (): Promise<{
 };
 interface State {
   data: {
-    [key: string]: { [key: string]: { key: string; value: number }[] };
+    [key: string]: { [key: string]: DataEntry[] };
   };
   language: string;
   column: string;
@@ -57,10 +60,12 @@ interface State {
 
 class BarChartForComparison extends React.Component<null, State> {
   d3Container: React.MutableRefObject<null>;
+  d3Container2: React.MutableRefObject<null>;
   sliderContainer: React.MutableRefObject<null>;
   constructor(props: null) {
     super(props);
     this.d3Container = React.createRef();
+    this.d3Container2 = React.createRef();
     this.sliderContainer = React.createRef();
     this.state = {
       data: {},
@@ -80,13 +85,18 @@ class BarChartForComparison extends React.Component<null, State> {
     const height = 600 - 2 * margin;
 
     if (this.d3Container.current) {
-      const dataSelected = this.state.data[this.state.language][this.state.column];
-      const x = d3.scaleBand().range([0, width]).padding(0.1);
-      x.domain(dateLabels).padding(0.5);
+      const dataSelected = this.state.data[this.state.language][
+        this.state.column
+      ];
+      const xExtent = d3.extent(dataSelected, (d) => d.date) as [Date, Date];
+      const x = d3
+        .scaleTime()
+        .range([margin, width + margin])
+        .domain(xExtent);
 
       const y = d3
         .scaleLinear()
-        .domain([0, 1.2 * (d3.max(dataSelected, (d) => d.value) || 0)])
+        .domain([0, 1.1 * (d3.max(dataSelected, (d) => d.value) || 0)])
         .range([height, 0]);
 
       d3.select(this.d3Container.current).html("");
@@ -111,9 +121,9 @@ class BarChartForComparison extends React.Component<null, State> {
       bars
         .enter()
         .append("rect")
-        .attr("x", (d) => x(d.key) || true)
+        .attr("x", (d) => x(d.date))
         .attr("y", (d) => y(d.value))
-        .attr("width", x.bandwidth())
+        .attr("width", 10)
         .attr("height", (d) => height - y(d.value))
         .attr("fill", (_, i) => barColor[i]);
       /*
@@ -158,6 +168,109 @@ class BarChartForComparison extends React.Component<null, State> {
         .append("g")
         .attr("transform", `translate(${margin},30)`);
       gRange.call(sliderRange);
+
+      /* test */
+      d3.select(this.d3Container2.current).html("");
+      const area = d3
+        .area()
+        .x((d) => {
+          const row = (d as unknown) as DataEntry;
+          return x(row.date);
+        })
+        .y1((d) => {
+          const row = (d as unknown) as DataEntry;
+          return y(row.date);
+        })
+        .y0(y(0));
+      const wrapper2 = d3
+        .select(this.d3Container2.current)
+        .attr("width", width + margin * 2)
+        .attr("height", height + margin * 2);
+
+      const svg2 = wrapper2
+        .append("g")
+        .attr("transform", `translate(${0},${margin})`);
+
+      const xAxis = d3.axisBottom(x);
+      const xAxisTranslate = height;
+
+      svg2
+        .append("g")
+        .attr("transform", `translate(0, ${xAxisTranslate})`)
+        .call(xAxis);
+
+      const yAxis = d3.axisLeft(y).tickFormat(d3.format("~s"));
+
+      svg2.append("g").attr("transform", `translate(${margin}, 0)`).call(yAxis);
+
+      svg2
+        .append("path")
+        // @ts-ignore weird type hints
+        .attr("d", area(dataSelected))
+        .attr("stroke", "#147F90")
+        .attr("stroke-width", "2px")
+        .attr("fill", "#A6E8F2");
+
+      // Interactivity
+
+      svg2.append("line").classed("hoverLine", true);
+      svg2.append("circle").classed("hoverPoint", true);
+      svg2.append("text").classed("hoverText", true);
+
+      svg2
+        .append("rect")
+        .attr("fill", "transparent")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", width + margin * 2)
+        .attr("height", height + margin * 2);
+
+      const mouseMove = (event: MouseEvent) => {
+        event.preventDefault();
+        const xCoord = event.pageX;
+
+        const mouseDate = x.invert(xCoord);
+        const mouseDateSnap = d3.timeMonth.floor(mouseDate);
+        const displayX = Math.min(x(mouseDateSnap), width + margin);
+        if (displayX < margin - width / dateLabels.length) {
+          return;
+        }
+
+        const bisectDate = d3.bisector((d: DataEntry) => d.date).right;
+        const xIndex = bisectDate(dataSelected, mouseDateSnap, 0);
+        const mousePopulation = dataSelected[xIndex].value;
+
+        svg2
+          .selectAll(".hoverLine")
+          .attr("x1", displayX)
+          .attr("y1", 0)
+          .attr("x2", displayX)
+          .attr("y2", height)
+          .attr("stroke", "#147F90")
+          .attr("fill", "#A6E8F2");
+
+        svg2
+          .selectAll(".hoverPoint")
+          .attr("cx", displayX)
+          .attr("cy", y(mousePopulation))
+          .attr("r", "7")
+          .attr("fill", "#147F90");
+
+        const isLessThanHalf = xIndex > dataSelected.length / 2;
+        const hoverTextX = isLessThanHalf ? "-0.75em" : "0.75em";
+        const hoverTextAnchor = isLessThanHalf ? "end" : "start";
+
+        svg2
+          .selectAll(".hoverText")
+          .attr("x", displayX)
+          .attr("y", y(mousePopulation))
+          .attr("dx", hoverTextX)
+          .attr("dy", "-1.25em")
+          .style("text-anchor", hoverTextAnchor)
+          .text(d3.format(".5s")(mousePopulation));
+      };
+
+      svg2.on("mousemove", mouseMove);
     }
 
     return (
@@ -191,6 +304,12 @@ class BarChartForComparison extends React.Component<null, State> {
             </div>
           </div>
         </div>
+        <svg
+          className="d3-component"
+          width={width}
+          height={height + margin * 2}
+          ref={this.d3Container2}
+        />
       </div>
     );
   }
--- app/src/components/BarChartForComparison/utils.ts
+++ app/src/components/BarChartForComparison/utils.ts
@@ -87,3 +87,21 @@ export const columnLabels = [
   "Max viewers",
   "Avg viewers",
 ];
+
+const months: { [key: string]: string } = {
+  january: "01",
+  february: "02",
+  march: "03",
+  april: "04",
+  may: "05",
+  june: "06",
+  july: "07",
+  august: "08",
+  september: "09",
+  october: "10",
+  november: "11",
+  december: "12",
+};
+
+export const getDate = (year: string, month: string): Date =>
+  new Date(`${year}-${months[month]}-01`);
