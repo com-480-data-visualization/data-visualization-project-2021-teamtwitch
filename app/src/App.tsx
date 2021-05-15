578668917a7265c6bff7dd87f62e5764f3052650
Huan-Cheng Chang <changhc84@gmail.com>
2021-05-15 08:09:35 +0200

    apply fullpage.js (#8)

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,15 +2,26 @@ import React from "react";
 import AreaChart from "src/components/AreaChart";
 import ChessLineChart from "src/components/ChessLineChart";
 import BubbleChart from "src/components/BubbleChart";
+import ReactFullpage from "@fullpage/react-fullpage";
 
 export default function App(): JSX.Element {
   return (
-    <div>
-      <BubbleChart />
-      <hr />
-      <AreaChart />
-      <hr />
-      <ChessLineChart />
-    </div>
+    <ReactFullpage
+      licenseKey="?"
+      scrollingSpeed={1000}
+      render={() => (
+        <ReactFullpage.Wrapper>
+          <div className="section">
+            <BubbleChart />
+          </div>
+          <div className="section">
+            <AreaChart />
+          </div>
+          <div className="section">
+            <ChessLineChart />
+          </div>
+        </ReactFullpage.Wrapper>
+      )}
+    />
   );
 }
