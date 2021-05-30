218d9cdddd14bb8ccafea8f180dbf78dee81e2e3
Huan-Cheng Chang <changhc84@gmail.com>
2021-05-30 21:43:05 +0200

    Scroll down button (#15)
    * scroll down button
    
    * comments

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,33 +7,47 @@ import ReactFullpage from "@fullpage/react-fullpage";
 import TitlePage from "src/components/TitlePage";
 import Page2 from "src/components/Page2";
 
+const ArrowIcon = require("./arrow.svg").default;
+const styles = require("./app.scss");
+
 export default function App(): JSX.Element {
   return (
-    <ReactFullpage
-      licenseKey="?"
-      scrollingSpeed={1000}
-      render={() => (
-        <ReactFullpage.Wrapper>
-          <div className="section">
-            <TitlePage />
-          </div>
-          <div className="section">
-            <Page2 />
-          </div>
-          <div className="section">
-            <BubbleChart />
-          </div>
-          <div className="section">
-            <AreaChart />
-          </div>
-          <div className="section">
-            <ChessLineChart />
-          </div>
-          <div className="section">
-            <ScatterPlot />
-          </div>
-        </ReactFullpage.Wrapper>
-      )}
-    />
+    <>
+      <div
+        className={styles.icon}
+        onClick={() =>
+          // @ts-ignore some global stuff brought by Fullpage
+          fullpage_api.moveSectionDown()
+        }
+      >
+        <ArrowIcon width="64px" height="64px" />
+      </div>
+      <ReactFullpage
+        licenseKey="?"
+        scrollingSpeed={1000}
+        render={() => (
+          <ReactFullpage.Wrapper>
+            <div className="section">
+              <TitlePage />
+            </div>
+            <div className="section">
+              <Page2 />
+            </div>
+            <div className="section">
+              <BubbleChart />
+            </div>
+            <div className="section">
+              <AreaChart />
+            </div>
+            <div className="section">
+              <ChessLineChart />
+            </div>
+            <div className="section">
+              <ScatterPlot />
+            </div>
+          </ReactFullpage.Wrapper>
+        )}
+      />
+    </>
   );
 }
